6ce30dc2b1ec836603bdc70056623551c7da9a71
Gleb121region <71015316+Gleb121region@users.noreply.github.com>
2023-12-28 21:29:21 +0300

    implemented a complex query using the Criteria API.

--- src/main/java/ru/spbstu/mvp/repository/FlatRepository.java
+++ src/main/java/ru/spbstu/mvp/repository/FlatRepository.java
@@ -1,45 +1,14 @@
 package ru.spbstu.mvp.repository;
 
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import ru.spbstu.mvp.entity.Flat;
-import ru.spbstu.mvp.request.flat.FlatRequest;
 
 //todo: нужно решить проблему с N+1, можно использовать https://www.baeldung.com/jpa-entity-graph
 // N+1 решает проблему, но есть самая главная проблема с получением данных из таблицы Photo поля photo_url
 
-// todo: нужно переписать под Criteria API
-
 @Repository
 public interface FlatRepository extends JpaRepository<Flat, Integer> {
-    @Query(value = """
-                SELECT DISTINCT f FROM Flat f
-                WHERE (:#{#request.city} is null or f.city = :#{#request.city})
-                AND (:#{#request.underground} is null or f.underground = :#{#request.underground})
-                AND (:#{#request.district} is null or f.district = :#{#request.district})
-                
-                AND (:#{#request.roomsCounts} is null or f.roomsCount in :#{#request.roomsCounts})
-                
-                AND (:#{#request.maxPricePerMonth} is null or f.pricePerMonth <= :#{#request.maxPricePerMonth})
-                AND (:#{#request.minPricePerMonth} is null or f.pricePerMonth >= :#{#request.minPricePerMonth})
-                   
-                AND (:#{#request.isRefrigerator} is null or f.isRefrigerator = :#{#request.isRefrigerator})
-                AND (:#{#request.isWashingMachine} is null or f.isWashingMachine = :#{#request.isWashingMachine})
-                AND (:#{#request.isTV} is null or f.isTV = :#{#request.isTV})
-                AND (:#{#request.isShowerCubicle} is null or f.isShowerCubicle = :#{#request.isShowerCubicle})
-                AND (:#{#request.isBathtub} is null or f.isBathtub = :#{#request.isBathtub})
-                AND (:#{#request.isFurnitureRoom} is null or f.isFurnitureRoom = :#{#request.isFurnitureRoom})
-                AND (:#{#request.isFurnitureKitchen} is null or f.isFurnitureKitchen = :#{#request.isFurnitureKitchen})
-                AND (:#{#request.isDishwasher} is null or f.isDishwasher = :#{#request.isDishwasher})
-                AND (:#{#request.isAirConditioning} is null or f.isAirConditioning = :#{#request.isAirConditioning})
-                AND (:#{#request.isInternet} is null or f.isInternet = :#{#request.isInternet})
-                ORDER BY f.id
-            """)
-    Page<Flat> findFlatByParams(@Param("request") FlatRequest request, Pageable pageable);
+
 }
 
--- src/main/java/ru/spbstu/mvp/service/FlatService.java
+++ src/main/java/ru/spbstu/mvp/service/FlatService.java
@@ -1,7 +1,15 @@
 package ru.spbstu.mvp.service;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.TypedQuery;
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Predicate;
+import jakarta.persistence.criteria.Root;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
@@ -14,6 +22,8 @@ import ru.spbstu.mvp.request.flat.FlatRequest;
 import ru.spbstu.mvp.response.flat.FlatResponse;
 import ru.spbstu.mvp.response.flat.FlatWithDescriptionResponse;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
 
@@ -24,9 +34,70 @@ public class FlatService {
     private final FlatRepository flatRepository;
     private final PhotoRepository photoRepository;
 
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    private Page<Flat> findFlatByParams(FlatRequest request, Pageable pageable) {
+        CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+        CriteriaQuery<Flat> criteriaQuery = criteriaBuilder.createQuery(Flat.class);
+        Root<Flat> flat = criteriaQuery.from(Flat.class);
+        List<Predicate> predicates = new ArrayList<>();
+        if (request.city() != null) {
+            predicates.add(criteriaBuilder.equal(flat.get("city"), request.city()));
+        }
+        if (request.underground() != null) {
+            predicates.add(criteriaBuilder.equal(flat.get("underground"), request.underground()));
+        }
+        if (request.district() != null) {
+            predicates.add(criteriaBuilder.equal(flat.get("district"), request.district()));
+        }
+        if (request.roomsCounts() != null) {
+            predicates.add(flat.get("roomsCount").in(request.roomsCounts()));
+        }
+        if (request.maxPricePerMonth() != null) {
+            predicates.add(criteriaBuilder.lessThanOrEqualTo(flat.get("pricePerMonth"), request.maxPricePerMonth()));
+        }
+        if (request.minPricePerMonth() != null) {
+            predicates.add(criteriaBuilder.greaterThanOrEqualTo(flat.get("pricePerMonth"), request.minPricePerMonth()));
+        }
+        if (request.isRefrigerator() != null) {
+            predicates.add(criteriaBuilder.equal(flat.get("isRefrigerator"), request.isRefrigerator()));
+        }
+        if (request.isWashingMachine() != null) {
+            predicates.add(criteriaBuilder.equal(flat.get("isWashingMachine"), request.isWashingMachine()));
+        }
+        if (request.isTV() != null) {
+            predicates.add(criteriaBuilder.equal(flat.get("isTV"), request.isTV()));
+        }
+        if (request.isShowerCubicle() != null) {
+            predicates.add(criteriaBuilder.equal(flat.get("isShowerCubicle"), request.isShowerCubicle()));
+        }
+        if (request.isBathtub() != null) {
+            predicates.add(criteriaBuilder.equal(flat.get("isBathtub"), request.isBathtub()));
+        }
+        if (request.isFurnitureRoom() != null) {
+            predicates.add(criteriaBuilder.equal(flat.get("isFurnitureRoom"), request.isFurnitureRoom()));
+        }
+        if (request.isFurnitureKitchen() != null) {
+            predicates.add(criteriaBuilder.equal(flat.get("isFurnitureKitchen"), request.isFurnitureKitchen()));
+        }
+        if (request.isDishwasher() != null) {
+            predicates.add(criteriaBuilder.equal(flat.get("isDishwasher"), request.isDishwasher()));
+        }
+        if (request.isAirConditioning() != null) {
+            predicates.add(criteriaBuilder.equal(flat.get("isAirConditioning"), request.isAirConditioning()));
+        }
+        if (request.isInternet() != null) {
+            predicates.add(criteriaBuilder.equal(flat.get("isInternet"), request.isInternet()));
+        }
+        criteriaQuery.where(predicates.toArray(new Predicate[0]));
+        TypedQuery<Flat> query = entityManager.createQuery(criteriaQuery);
+        return new PageImpl<>(query.getResultList(), pageable, query.getResultList().size());
+    }
+
+
     public Set<FlatResponse> getFlatsInfo(FlatRequest request, Integer limit, Integer offset) {
-        Pageable pageable = PageRequest.of(offset, limit);
-        Page<Flat> flats = flatRepository.findFlatByParams(request, pageable);
+        Page<Flat> flats = findFlatByParams(request, PageRequest.of(offset, limit));
         return flats.map(
                 flat -> FlatResponse.builder()
                         .id(flat.getId())
