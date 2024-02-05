01219614e8cf5dd3f36ff1f967963dbd8c8b85e2
Gleb121region <71015316+Gleb121region@users.noreply.github.com>
2024-02-05 22:25:56 +0300

    modify repository

--- src/main/java/ru/spbstu/mvp/repository/PhotoRepository.java
+++ src/main/java/ru/spbstu/mvp/repository/PhotoRepository.java
@@ -1,6 +1,7 @@
 package ru.spbstu.mvp.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Repository;
 import ru.spbstu.mvp.entity.Flat;
 import ru.spbstu.mvp.entity.Photo;
@@ -9,6 +10,5 @@ import java.util.Set;
 
 @Repository
 public interface PhotoRepository extends JpaRepository<Photo, Integer> {
-
-    Set<Photo> findPhotosByFlat(Flat flat);
+    Set<Photo> findPhotosByFlatId(@NonNull Integer id);
 }
