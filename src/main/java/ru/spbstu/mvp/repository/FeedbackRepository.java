e0d364842640ada855be7b21ddae60191fecf6d4
Gleb121region <71015316+Gleb121region@users.noreply.github.com>
2024-02-05 22:24:48 +0300

    modify repositories

--- src/main/java/ru/spbstu/mvp/repository/FeedbackRepository.java
+++ src/main/java/ru/spbstu/mvp/repository/FeedbackRepository.java
@@ -1,7 +1,17 @@
 package ru.spbstu.mvp.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.lang.NonNull;
+import org.springframework.stereotype.Repository;
 import ru.spbstu.mvp.entity.Feedback;
+import ru.spbstu.mvp.entity.User;
+import ru.spbstu.mvp.entity.enums.FeedbackType;
 
+import java.util.Set;
+
+@Repository
 public interface FeedbackRepository extends JpaRepository<Feedback, Integer> {
+
+    Set<Feedback> findByFeedbackTypeAndUser(@NonNull FeedbackType feedbackType, @NonNull User user);
+
 }
--- src/main/java/ru/spbstu/mvp/repository/FlatRepository.java
+++ src/main/java/ru/spbstu/mvp/repository/FlatRepository.java
@@ -4,11 +4,15 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import ru.spbstu.mvp.entity.Flat;
 
+import java.util.Optional;
+
 //todo: нужно решить проблему с N+1, можно использовать https://www.baeldung.com/jpa-entity-graph
 // N+1 решает проблему, но есть самая главная проблема с получением данных из таблицы Photo поля photo_url
 
 @Repository
 public interface FlatRepository extends JpaRepository<Flat, Integer> {
 
+    @Override
+    Optional<Flat> findById(Integer integer);
 }
 
