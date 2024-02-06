f73b9a0e8a7155b38999859607ede3d763a7547b
Gleb121region <71015316+Gleb121region@users.noreply.github.com>
2024-02-06 13:33:06 +0300

    create new Exception file

---
+++ src/main/java/ru/spbstu/mvp/exception/PhotoUploadException.java
@@ -0,0 +1,7 @@
+package ru.spbstu.mvp.exception;
+
+public class PhotoUploadException extends RuntimeException {
+    public PhotoUploadException(String message) {
+        super(message);
+    }
+}
