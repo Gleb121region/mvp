5d8fcdaa6159459eaf4c365db13ecd56ca4d8575
Gleb121region <71015316+Gleb121region@users.noreply.github.com>
2024-02-06 09:05:47 +0300

    refactor controllers

--- src/main/java/ru/spbstu/mvp/controller/FeedbackController.java
+++ src/main/java/ru/spbstu/mvp/controller/FeedbackController.java
@@ -1,13 +1,13 @@
 package ru.spbstu.mvp.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import ru.spbstu.mvp.request.feedback.CreateFeedbackRequest;
-import ru.spbstu.mvp.response.flat.FlatLikedResponse;
+import ru.spbstu.mvp.response.flat.FlatResponse;
 import ru.spbstu.mvp.service.FeedbackService;
+
+import java.security.Principal;
+import java.util.Set;
 
 @RestController
 @RequestMapping("/api/v1/feedback")
@@ -16,14 +16,15 @@ public class FeedbackController {
 
     private final FeedbackService feedbackService;
 
-    @GetMapping
-    public FlatLikedResponse getLikedFeedbackByUser() {
-        return null;
+    @GetMapping("/liked")
+    @ResponseBody
+    public Set<FlatResponse> getLikedFeedbackByUser(Principal connectedUser) {
+        return feedbackService.getLikeFeedbacks(connectedUser);
     }
 
-    @PostMapping
-    public void createFeedback(CreateFeedbackRequest request) {
-        feedbackService.createFeedback(request);
+    @PostMapping("/create")
+    public void createFeedback(CreateFeedbackRequest request, Principal connectedUser) {
+        feedbackService.createFeedback(request, connectedUser);
     }
 
 }
