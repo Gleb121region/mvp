a6fcdc0aaf535ba6e097a68333abb790a4ff55d1
Gleb121region <71015316+Gleb121region@users.noreply.github.com>
2024-02-06 13:33:48 +0300

    Implemented uploadFlatPhotos

--- src/main/java/ru/spbstu/mvp/service/PhotoService.java
+++ src/main/java/ru/spbstu/mvp/service/PhotoService.java
@@ -7,35 +7,56 @@ import com.backblaze.b2.client.contentSources.B2ContentTypes;
 import com.backblaze.b2.client.contentSources.B2FileContentSource;
 import com.backblaze.b2.client.exceptions.B2Exception;
 import com.backblaze.b2.client.structures.B2UploadFileRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
+import ru.spbstu.mvp.entity.Flat;
+import ru.spbstu.mvp.entity.Photo;
+import ru.spbstu.mvp.exception.FlatNotFoundException;
+import ru.spbstu.mvp.exception.PhotoUploadException;
+import ru.spbstu.mvp.repository.FlatRepository;
+import ru.spbstu.mvp.repository.PhotoRepository;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.List;
+import java.util.Optional;
 
+@Service
+@RequiredArgsConstructor
 public class PhotoService {
     private static final String USER_AGENT = "Gleb"; // как я понимаю любой
     private static final String APP_KEY = "K003xFf2gDsRRZSg+vq/ZG/zC1rcFqo"; // applicationKey:
     private static final String APP_KEY_ID = "0035c3c5567d6ce0000000005"; // keyID
     private static final String bucketId = "658c639c65a5e6278da60c1e"; //  Сегмент кода:
 
-    public String uploadPhoto(MultipartFile file) {
+    private static final String bucketUrl = "https://f003.backblazeb2.com/file/gleb-bucket/";
+
+    private final PhotoRepository photoRepository;
+    private final FlatRepository flatRepository;
+
+    private void uploadFlatPhoto(MultipartFile photo, Flat flat) {
         B2StorageClient client = B2StorageClientFactory.createDefaultFactory().create(APP_KEY_ID, APP_KEY, USER_AGENT);
         try {
             File tempFile = File.createTempFile("upload", null);
-            file.transferTo(tempFile);
-
+            photo.transferTo(tempFile);
             final B2ContentSource source = B2FileContentSource.build(tempFile);
-            final String fileName = "demo/" + file.getOriginalFilename();
-            B2UploadFileRequest request = B2UploadFileRequest
-                    .builder(bucketId, fileName, B2ContentTypes.B2_AUTO, source)
-                    .setCustomField("color", "blue")
-                    .build();
-            var file1 = client.uploadSmallFile(request);
-            System.out.println("uploaded " + file1);
+            final String fileName = "demo/" + photo.getOriginalFilename();
+            B2UploadFileRequest request = B2UploadFileRequest.builder(bucketId, fileName, B2ContentTypes.B2_AUTO, source).setCustomField("color", "blue").build();
+            client.uploadSmallFile(request);
+            Photo currentPhoto = Photo.builder().photoUrl(bucketUrl + fileName).flat(flat).build();
+            photoRepository.save(currentPhoto);
+        } catch (B2Exception | IOException e) {
+            throw new PhotoUploadException("Photo upload exception");
+        }
+    }
 
-            return "File uploaded successfully: " + fileName;
-        } catch (IOException | B2Exception e) {
-            return "Error occurred while uploading file: " + e.getMessage();
+    public void uploadFlatPhotos(List<MultipartFile> photos, int flatId) {
+        Optional<Flat> optionalFlat = flatRepository.findById(flatId);
+        if (optionalFlat.isPresent()) {
+            photos.forEach(photo -> uploadFlatPhoto(photo, optionalFlat.get()));
+        } else {
+            throw new FlatNotFoundException("Flat not found");
         }
     }
 }
